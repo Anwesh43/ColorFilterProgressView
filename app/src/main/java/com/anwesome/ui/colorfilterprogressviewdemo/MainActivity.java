ddd07122a4fff2d2100337741272f519bdf7d36f
Anwesh43 <anweshthecool0@gmail.com>
2017-05-14 08:42:46 +0530

    using colorfilterprogressview in app

--- app/src/main/java/com/anwesome/ui/colorfilterprogressviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/colorfilterprogressviewdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.anwesome.ui.colorfilterprogressviewdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.colorfilterprogressview.ColorFilterProgressView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.forest2);
+        ColorFilterProgressView colorFilterProgressView = new ColorFilterProgressView(this,bitmap);
+        addContentView(colorFilterProgressView,new ViewGroup.LayoutParams(300,300));
     }
 }
