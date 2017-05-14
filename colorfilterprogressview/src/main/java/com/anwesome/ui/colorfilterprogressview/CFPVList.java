6beb26b100e0d1866509ac890e5582a5393ccbb8
Anwesh43 <anweshthecool0@gmail.com>
2017-05-14 09:06:49 +0530

    Adding CFPVList

---
+++ colorfilterprogressview/src/main/java/com/anwesome/ui/colorfilterprogressview/CFPVList.java
@@ -0,0 +1,31 @@
+package com.anwesome.ui.colorfilterprogressview;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+/**
+ * Created by anweshmishra on 14/05/17.
+ */
+public class CFPVList {
+    private Activity activity;
+    private ScrollView scrollView;
+    private CFPVLayout cfpvLayout;
+    private boolean isShown = false;
+    public CFPVList(Activity activity) {
+        this.activity = activity;
+        cfpvLayout = new CFPVLayout(activity);
+        scrollView = new ScrollView(activity);
+        scrollView.addView(cfpvLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+    public void addImage(Bitmap bitmap,int...color) {
+
+    }
+    public void show() {
+        if(!isShown) {
+            activity.setContentView(cfpvLayout);
+            isShown = true;
+        }
+    }
+}
